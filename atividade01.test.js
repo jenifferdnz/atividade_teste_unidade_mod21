3e96153c69e6d4b04d6ff82d35d12f3fa5d4c000
Jeniffer Dinz <73167898+jenifferdnz@users.noreply.github.com>
2023-05-23 09:43:58 -0300

    Organizando arquivos

--- atividade01.test.js
+++
@@ -1,7 +0,0 @@
-const {mdcnum} = require('./atividade01')
-describe ('Funcão atividade 01', () => {
-    it ('MDC', () => {
-        expect(mdcnum(10, 20)).toBe(10)
-        expect(mdcnum(5, 10)).toBe(5)
-    })
-})
